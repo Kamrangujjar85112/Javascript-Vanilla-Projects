67d598e0ba59a812a0a7d7329096b00bcfed2f9f
Muhammad Kamran Siddique <96736763+Kamrangujjar85112@users.noreply.github.com>
2022-01-31 09:48:03 +0500

    Delete setup/.history directory

--- setup/.history/01-color-flipper/setup/app_20220122114023.js
+++
@@ -1,9 +0,0 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
-btn.addEventListener('click' , function(){
-    // random number btw 0 and 3[3]
-    const randomNumber = 2;
-    document.body.style.backgroundColor = color[randomNumber];
-    color.textContent = colors[randomNumber];
-})
